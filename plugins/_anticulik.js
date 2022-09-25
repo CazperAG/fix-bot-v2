2d973c8a023766439e47b994678c89c468a008b5
CazperAG <113819646+CazperAG@users.noreply.github.com>
2022-09-25 09:50:09 +0700

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -5,13 +5,13 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `「 WH-MODS-BOT-V1 」
+    let teks = `「 CazperBotz-V3 」
 
     ╭━━━━「 SEWA 」
-    ┊⫹⫺ Hemat: 5k/grup (1 minggu)
-    ┊⫹⫺ Normal: 15k/grup (1 bulan)
-    ┊⫹⫺ Standar: 30k/grup (2 bulan)
-    ┊⫹⫺ Pro: 35k/grup (4 bulan)                                                      
+    ┊⫹⫺ Hemat: 5k/grup (2 minggu)
+    ┊⫹⫺ Normal: 15k/grup (2 bulan)
+    ┊⫹⫺ Standar: 30k/grup (3 bulan)
+    ┊⫹⫺ Pro: 35k/grup (5 bulan)                                                      
     ┊⫹⫺ Vip: = 65k/grup (12 bulan)
     ╰═┅═━––––––๑
     
@@ -20,22 +20,22 @@ handler.all = async function (m, { isBlocked }) {
     ┊⫹⫺ Normal: 20k (1 bulan)
     ┊⫹⫺ Pro: 40k (4 bulan)
     ┊⫹⫺ Vip: 50k (8 bulan)                                               
-    ┊⫹⫺ Permanent: = 70k (Unlimited)
+    ┊⫹⫺ Permanent: = 80k (Unlimited)
     ╰═┅═━––––––๑
     
     ⫹⫺ PAYMENT:
-    • Pulsa Telkomsel: [082127487538]
-    • Dana: [082127487538]
-    • Gopay: [082127487538]
-    • Ovo: [082127487538]
-    • Link Aja: [082127487538]
+    • Pulsa Smartfren: [08886529913]
+    • Dana: [08886529913]
+    • Gopay: [08886529913]
+    • Ovo: [08886529913]
+    • Link Aja: [08886529913]
     
     Nomor Owner :
-    wa.me/6282127487538
+    wa.me/628886529913
     
     ▌│█║▌║▌║║▌║▌║█│▌
     
-    #BY WH MODS DEV`
+    #BY CASPERAGARIO DEV`
     this.reply(m.chat, teks, m)
     const data = global.owner.filter(([id, isCreator]) => id && isCreator)
     this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
