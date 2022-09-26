e39a7c19f27d4550e560b7aab6ffee23799614f3
CazperAG <113819646+CazperAG@users.noreply.github.com>
2022-09-26 14:23:08 +0700

    Update owner-getplugins.js

--- plugins/owner-getplugins.js
+++ plugins/owner-getplugins.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+const fs = require('fs')
+const path = require('path')
 let handler = async (m, { usedPrefix, command, text }) => {
     if (!text) throw `where is the text?\n\nexempel: ${usedPrefix + command} menu`
     const filename = path.join(__dirname, `./${text}${!/\.js$/i.test(text) ? '.js' : ''}`)
@@ -14,6 +14,6 @@ handler.help = ['getplugin'].map(v => v + ' [filename]')
 handler.tags = ['owner']
 handler.command = /^(getplugin|get ?plugin|gp)$/i
 
-handler.owner = true
+handler.rowner = true
 
-export default handler
+module.exports = handler
